d82b2fe07a87617dc291b5a008e4115be9bc5e7d
sameena <sameenah.tm@gmail.com>
2022-06-11 21:35:22 +0200

    Updated the contact page with styling

--- Contact.py
+++ Contact.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python3
 
-# Import modules for CGI handling 
-import cgi, cgitb 
-# Create instance of FieldStorage
-form = cgi.FieldStorage()
-
-# Get data from fields
-if form.getvalue('subject'):
-   subject = form.getvalue('subject')
-else:
-   subject = "Not set"
 
 
 print ("Content-type:text/html\r\n\r\n")
@@ -18,17 +8,34 @@ print ("<head>")
 
 print ("<style>")
 #header styling
-# print ("""
-# .images{
-#     width: 100%;
-#     height: 200px;
-#     background-image: url('Pic.gif') ;
-#     background-size: cover;
-#     padding-left: 80px;
+print ("""
+.images{
+    width: 100%;
+    height: 200px;
+    background-image: url('Pic.gif') ;
+    background-size: cover;
+    padding-left: 80px;
     
-# }
+}
+#container1{
+display:flex;
+flex-direction:column;
+align-item:flex-start;
+border:1px solid black;
+padding-top:20px;
+-webkit-box-shadow: 5px 5px 15px 5px #000000; 
+box-shadow: 5px 5px 15px 5px #000000;
 
-# """)
+}
+.container4{
+margin-bottom:10px;
+padding_bottom:10px;
+           }
+#container3{
+margin-top:70px;
+}
+
+""")
 #########
 
 print ("div.container { width:100%; border: 1px solid grey;}")
@@ -37,7 +44,7 @@ print ("li{float:left;}")
 print ("li a {display: block;color: white;text-align: center;padding: 16px;font-size:20px; text-decoration: none;}")
 print ("li a:hover { background-color: #111111;}")
 print (".footer { position: fixed; left: 0; bottom: 0; width: 100%; height:60px;  background-color: #808080; color: white; text-align: center; }")
-print (" #container1{ width:1000px; height=1500px; line-height:1.6;}")
+print (" #container1{ width:900px; height=1500px; line-height:0;}")
 
 #style for divindg into 2 columns
 
@@ -48,7 +55,7 @@ print (".row:after {content: "";display: table;clear: both;}")
 
 print ("</style>")
 
-print ("<title>MSALigMap</title>")
+print ("<title>LiBiSCo</title>")
 print ("</head>")
 
 
@@ -74,41 +81,56 @@ print ("<h1> Please send any comments or suggestions about the MSALigMap server 
 print ("<div id='container1'>")
 
 
-print ("<IMG SRC='HA.jpg' ALIGN='top' width=120px height=120px >")
+
+print("<div  >")
+
+print ("<IMG SRC='HA.jpg' ALIGN='top' width=120px height=120px  style='border-radius:15px';>")
+print("<div>")
+
+
+print("<div class='container4'>")
 print ("<p><b>Professor Henrik Aronsson</b></p>")
 print  ("<p>Head of Department</p>")
 print ("<p>Department of Biological & Environmental Sciences</p>")
 print ("<p>Email: henrik.aronsson@bioenv.gu.se</p>")
+print("<div>")
 
 
 
-print ("<div class='row'>")
-print ("  <div class='column'>")
-print ("<IMG SRC='SH3.jpeg' ALIGN='top' width=120px height=120px >")
+print("<div  id='container3'>")
+print ("<IMG SRC='SH3.jpeg' ALIGN='top' width=120px height=120px style='border-radius:15px';>")
+print("<div>")
+
+print("<div class='container4'>")
 print ("<p><b>Sameer Hassan</b></p>")
-print  ("<p>Researcher</p>")
+print  ("<p>Postdoc Researcher</p>")
 print ("<p>Department of Biosciences and Nutrition</p>")
 print ("<p>Email: sameer.hassan@ki.se</p>")
 print ("</div>")
 
-print ("<div class='column'>")
-print ("<IMG SRC='MT.jpg' ALIGN='top' width=120px height=120px >")
+print("<div id='container3'>")
+
+print ("<IMG SRC='MT.jpg' ALIGN='top' width=120px height=120px style='border-radius:15px';>")
+print("<div>")
+print("<div class='container4'>")
 print ("<p><b>Mats Topel</b></p>")
 print  ("<p>Researcher</p>")
 print ("<p>Department of Marine Sciences</p>")
 print ("<p>Email: mats.topel@marine.gu.se</p>")
 print ("</div>")
 
-print ("  <div class='column'>")
-print ("<IMG SRC='Sameena.jpeg' ALIGN='top' width=120px height=120px >")
+print("<div id='container3'>")
+print ("<IMG SRC='SH3.jpeg' ALIGN='top' width=120px height=120px  style='border-radius:15px';>")
+print("<div>")
+print("<div class='container4'>")
 print ("<p><b>Haleemath Sameena Sameer</b></p>")
 print  ("<p>Master Student, Department of Computer and Systems Sciences</p>")
 print ("<p>Stockholm University</p>")
 print ("<p>Email: sameenah.tm@gmail.com </p>")
 print ("</div>")
+
 print ("</div>")
 print ("</div>")
 
 print ("</body>")
 print ("</html>")
-
